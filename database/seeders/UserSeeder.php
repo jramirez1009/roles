f1e71bee9b2f74fef7f847fced8d9dc328b846e4
Jesús Ramírez <jramirez1009@hotmail.com>
2023-09-07 16:04:37 -0500

    comprando que si funciona los permisos con directiva @can

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       User::create([
+        'name' => 'superadmin',
+        'email' => 'superadmin@role.com',
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+       ])->assignRole('superadmin');
+
+       User::create([
+        'name' => 'admin',
+        'email' => 'admin@role.com',
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+       ])->assignRole('admin');
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @can('dashboard')
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("view admin!") }}
                 </div>
+                @endcan
+                @can('list')
+                    <div class="p-6 text-gray-900">
+                        {{ __("view usuario!") }}
+                    </div>
+                @endcan
+                
+                
             </div>
         </div>
     </div>
